6fc7750fc14b9318969cf6a67949dd6431863db5
garen <karmartir@gmail.com>
2025-03-14 03:11:47 -0400

    4st commit navbar style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,17 +3,18 @@ import { NavLink } from "react-router-dom";
 
 export default function CustomNavbar() {
 	return (
-		<nav className="navbar navbar-expand-lg navbar-dark bg-transparent w-100 position-fixed top-0">
+		<nav className="navbar navbar-expand-lg navbar-dark bg-transparent w-100 position-fixed top-0 mt-5">
 			<Container>
 				<Nav className="ms-auto">
-					<Nav.Link as={NavLink} to="/" className="text-white">Main</Nav.Link>
-					<Nav.Link as={NavLink} to="/intro" className="text-white">Intro</Nav.Link>
-					<Nav.Link as={NavLink} to="/about" className="text-white">About</Nav.Link>
-					<Nav.Link as={NavLink} to="/success-stories" className="text-white">Success Stories</Nav.Link>
-					<Nav.Link as={NavLink} to="/investments" className="text-white">Investments</Nav.Link>
-					<Nav.Link as={NavLink} to="/contact" className="text-white">Contact</Nav.Link>
+					<Nav.Link as={NavLink} to="/" className="fw-normal text-white" style={{ fontFamily: "Julius Sans One", fontSize: "28.7px", lineHeight: "100%" }}>Main</Nav.Link>
+					<Nav.Link as={NavLink} to="/intro" className="fw-normal text-white" style={{ fontFamily: "Julius Sans One", fontSize: "28.7px", lineHeight: "100%" }}>Intro</Nav.Link>
+					<Nav.Link as={NavLink} to="/about" className="fw-normal text-white" style={{ fontFamily: "Julius Sans One", fontSize: "28.7px", lineHeight: "100%" }}>About</Nav.Link>
+					<Nav.Link as={NavLink} to="/success-stories" className="fw-normal text-white" style={{ fontFamily: "Julius Sans One", fontSize: "28.7px", lineHeight: "100%" }}>Success Stories</Nav.Link>
+					<Nav.Link as={NavLink} to="/investments" className="fw-normal text-white" style={{ fontFamily: "Julius Sans One", fontSize: "28.7px", lineHeight: "100%" }}>Investments</Nav.Link>
+					<Nav.Link as={NavLink} to="/contact" className="fw-normal text-white" style={{ fontFamily: "Julius Sans One", fontSize: "28.7px", lineHeight: "100%" }}>Contact</Nav.Link>
 				</Nav>
 			</Container>
 		</nav>
 	);
 }
+
