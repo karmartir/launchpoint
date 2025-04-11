0d82f14a5a421ea7b835ef284ad29a1defc4534a
garen <karmartir@gmail.com>
2025-04-11 15:58:31 -0400

    added logos in investments

--- src/pages/Investments.jsx
+++ src/pages/Investments.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Container} from 'react-bootstrap';
+import investLogos from '../images/investment-logos.png';
+
 
 const Investments = () => {
 	return (
 
-		<div className="d-flex flex-column justify-content-center align-items-center vh-100 text-white">
-			<Container>
+		<div className="d-flex flex-column justify-content-center align-items-center vh-100 text-white mt-4">
+			<Container style={{backgroundColor: 'rgba(128, 108, 118, 0.5)', padding: '25px', marginTop: '150px', borderRadius: '15px', paddingLeft: '50px' }}>
 				<h2 className="fw-normal text-center"  >Our Partners</h2>
-				<img src={Investments} alt="Investments" />
+				<img src={investLogos} alt="Investments" />
 			</Container>
 
 		</div>
