b5224f67eefd127f29e09b962b070ce46121d689
garen <karmartir@gmail.com>
2025-04-15 10:23:00 -0400

    logos on invesments changed color

--- src/pages/Investments.jsx
+++ src/pages/Investments.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Container} from 'react-bootstrap';
-import investLogos from '../images/investment-logos.png';
+import investLogos from '../images/logos.png';
+import first from '../images/logos/1.svg';
+import fourth from '../images/logos/4.jpg';
+import Table from 'react-bootstrap/Table';
 
 
 const Investments = () => {
@@ -8,6 +11,41 @@ const Investments = () => {
 
 		<div className="d-flex flex-column justify-content-center align-items-center vh-100 text-white mt-4">
 			<Container style={{ padding: '25px', marginTop: '150px', borderRadius: '15px', paddingLeft: '50px' }}>
+
+				{/*<Table  bordered hover>*/}
+				{/*	<thead>*/}
+				{/*	<tr>*/}
+				{/*		<th>*/}
+				{/*			<img src={first} alt="uhaul"/>*/}
+				{/*			</th>*/}
+				{/*		<th>	<img className='small' src={fourth} alt="fourth"/></th>*/}
+				{/*		<th>Last Name</th>*/}
+
+				{/*	</tr>*/}
+				{/*	</thead>*/}
+				{/*	<tbody>*/}
+				{/*	<tr>*/}
+
+				{/*		<td>Mark</td>*/}
+				{/*		<td>Otto</td>*/}
+				{/*		<td>@mdo</td>*/}
+				{/*	</tr>*/}
+				{/*	<tr>*/}
+
+				{/*		<td>Jacob</td>*/}
+				{/*		<td>Thornton</td>*/}
+				{/*		<td>@fat</td>*/}
+				{/*	</tr>*/}
+				{/*	<tr>*/}
+
+				{/*		<td colSpan={2}>Larry the Bird</td>*/}
+				{/*		<td>@twitter</td>*/}
+				{/*	</tr>*/}
+				{/*	</tbody>*/}
+				{/*</Table>*/}
+
+
+
 				<h2 className="fw-normal text-center"  >Our Partners</h2>
 				<img src={investLogos} alt="Investments" />
 			</Container>
