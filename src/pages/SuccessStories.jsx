bb991cca36cbfdce1a9e7f753ce7fc4da9c5e23a
garen <karmartir@gmail.com>
2025-04-19 13:14:41 -0400

    4 in row

--- src/pages/SuccessStories.jsx
+++ src/pages/SuccessStories.jsx
@@ -5,25 +5,23 @@ import './SuccessStories.css'; // Import the new CSS file
 
 const SuccessStories = () => {
 
-
     return (
-        <Container className="text-center success-stories-container">
+			<Container className="text-center success-stories-container" style={{ maxWidth: "1140px" }}>
             <div className="ms-5">
                 <h2 className="success-stories-title">
                     <i>Success stories of Startups Fueled by Our Support</i>
                 </h2>
             </div>
 
-            <Row className="success-stories-row mt-4 g-3 justify-content-center" style={{ marginTop: '20px'}}>
+            <Row className="success-stories-row mt-4 g-3" style={{ marginTop: '20px' }}>
                 {Object.keys(peopleData).map((key) => {
                     const person = peopleData[key];
                     return (
-                    <Col lg={2} md={4} sm={6} xs={8} className="success-stories-column" key={person.id}>
-                        <NavLink to={`/success-stories/${person.id}`} style={{ textDecoration: 'none' }}>
+                        <Col lg={3} md={6} sm={12} className="success-stories-column" key={person.id}>
+                            <NavLink to={`/success-stories/${person.id}`} style={{ textDecoration: 'none' }}>
                                 <img src={person.img} alt={person.name} className="img-fluid success-stories-image" />
                                 <div className="success-stories-info mt-1">
                                     <h6 className="text-white">{person.name}</h6>
-
                                 </div>
                             </NavLink>
                         </Col>
