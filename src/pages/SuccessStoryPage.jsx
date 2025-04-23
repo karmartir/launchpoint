5470f4dee3a541682855d159f58cf8f50d1d8609
garen <karmartir@gmail.com>
2025-04-23 12:31:15 -0400

    success story page text and name color change to darker

--- src/pages/SuccessStoryPage.jsx
+++ src/pages/SuccessStoryPage.jsx
@@ -6,7 +6,7 @@ import {peopleData} from '../data/PeopleData.jsx';
 const SuccessStoryPage = () => {
 	const { personId } = useParams();
 	const person = peopleData[personId];
-
+	const styles = {fontSize: '1.1rem', fontWeight: 'lighter', color: 'rgba(255,253,253,0.62)'}
 	if (!person) return <h2 className="text-center mt-5">Person not found.</h2>;
 
 	return (
@@ -16,9 +16,9 @@ const SuccessStoryPage = () => {
 					<img src={person.img} alt={person.name} className="img-fluid" style={{ borderRadius: "10px", maxWidth: "100%" }} />
 				</div>
 				<div className="col-md-7 bio-text">
-					<h2>{person.name}, ({person.country})</h2>
+					<h2 style={{ color: "rgba(224,224,224,0.82)"}}>{person.name}, ({person.country})</h2>
 					<h4 style={{ color: "rgba(224,224,224,0.82)", marginBottom: '30px', fontSize: '1.2rem' }}>{person.title}</h4>
-					<p className="mt-3 bio-text" style={{fontSize: '1.1rem', fontWeight: 'lighter'}}>{person.description}</p>
+					<p className="mt-3 bio-text" style={styles}>{person.description}</p>
 
 				</div>
 				<div className="mt-4 text-end">
